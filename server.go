4c7a3d8fea4e50480344b3b31bc3e010cffcfdd6
666savages <egor0263s@gmail.com>
2021-11-14 18:08:55 +0500

    141121.add.save.message.view

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,13 +11,24 @@ func indexView(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", nil)
 }
 
+func saveMessageView(c *gin.Context) {
+	message := c.PostForm("message")
+	key := keyBuilder.Get()
+	keeper.Set(key, message)
+	c.HTML(http.StatusOK, "key.html", gin.H{"key": fmt.Sprintf("http://%s/%s", c.Request.Host, key)})
+}
+
 func getRouter() *gin.Engine {
 	router := gin.Default()
-	router.LoadHTMLFiles("templates/index.html")
+	router.LoadHTMLFiles("templates/index.html", "templates/key.html")
 	router.GET("/", indexView)
+	router.POST("/", saveMessageView)
 	return router
 }
 func main() {
 	router := getRouter()
 	router.Run("localhost:8080")
 }
+
+// GO RUN .
+// TODO TEST 1 57 07
--- server_test.go
+++ server_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -26,9 +27,22 @@ func TestSaveMessage(t *testing.T) {
 	testMessage := "foo"
 	postData := strings.NewReader(fmt.Sprintf("message=%s", testMessage))
 	request, _ := http.NewRequest("POST", "/", postData)
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	w := httptest.NewRecorder()
 	handleTestRequest(w, request)
 	if w.Code != 200 {
 		t.Error("save is not 200")
+	}
+
+	key := keyBuilder.Get()
+	savedMessage, _ := keeper.Get(key)
+	if savedMessage != testMessage {
+		t.Error("message was not saved")
+	}
+	result := w.Result()
+	defer result.Body.Close()
+	data, _ := ioutil.ReadAll(result.Body)
+	if !strings.Contains(string(data), key) {
+		t.Error("error: page without key")
 	}
 }
